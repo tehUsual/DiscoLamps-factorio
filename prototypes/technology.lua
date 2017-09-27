1ee57ea5fa84e0f57427026270ed4218b8bd07a0
tehUsual <haugland54@gmail.com>
2017-09-27 21:46:27 +0200

    added technology

---
+++ prototypes/technology.lua
@@ -0,0 +1,71 @@
+data:extend({
+    {
+        type = "technology",
+        name = "small-color-lamp-t",
+        icon_size = 128,
+        prerequisites = { "optics" },
+        effects = {
+            {
+                type = "unlock-recipe",
+                recipe = "rgb-circuit-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-red-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-blue-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-green-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-lime-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-yellow-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-pink-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-cyan-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-grey-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-black-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-orange-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-purple-c"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = "small-lamp-brown-c"
+            },
+        },
+        unit = {
+            count = 20,
+            ingredients =
+            {
+                { "science-pack-1", 1 }
+            },
+            time = 15
+        },
+        order = "a-h-a"
+    }
+})
