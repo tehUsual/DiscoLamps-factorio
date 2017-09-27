9d097eca7929513e323cfd93fef33b97660542bf
tehUsual <haugland54@gmail.com>
2017-09-27 17:29:03 +0200

    project upload

---
+++ prototypes/items.lua
@@ -0,0 +1,134 @@
+data:extend({
+    -- Red Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-red",
+        icon = "__DiscoLamps__/graphics/small-lamp-red.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-red]",
+        place_result = "small-lamp-red",
+        stack_size = 50
+    },
+    -- Blue Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-blue",
+        icon = "__DiscoLamps__/graphics/small-lamp-blue.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-blue]",
+        place_result = "small-lamp-blue",
+        stack_size = 50
+    },
+    -- Green Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-green",
+        icon = "__DiscoLamps__/graphics/small-lamp-green.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-green]",
+        place_result = "small-lamp-green",
+        stack_size = 50
+    },
+    -- Lime Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-lime",
+        icon = "__DiscoLamps__/graphics/small-lamp-lime.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-lime]",
+        place_result = "small-lamp-lime",
+        stack_size = 50
+    },
+    -- Yellow Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-yellow",
+        icon = "__DiscoLamps__/graphics/small-lamp-yellow.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-yellow]",
+        place_result = "small-lamp-yellow",
+        stack_size = 50
+    },
+    -- Pink Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-pink",
+        icon = "__DiscoLamps__/graphics/small-lamp-pink.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-pink]",
+        place_result = "small-lamp-pink",
+        stack_size = 50
+    },
+    -- Cyan Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-cyan",
+        icon = "__DiscoLamps__/graphics/small-lamp-cyan.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-cyan]",
+        place_result = "small-lamp-cyan",
+        stack_size = 50
+    },
+    -- Grey Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-grey",
+        icon = "__DiscoLamps__/graphics/small-lamp-grey.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-grey]",
+        place_result = "small-lamp-grey",
+        stack_size = 50
+    },
+    -- Black Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-black",
+        icon = "__DiscoLamps__/graphics/small-lamp-black.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-black]",
+        place_result = "small-lamp-black",
+        stack_size = 50
+    },
+    -- Orange Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-orange",
+        icon = "__DiscoLamps__/graphics/small-lamp-orange.png",
+        flags = { "goes-to-quickbar" },
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-orange]",
+        place_result = "small-lamp-orange",
+        stack_size = 50
+    },
+    -- Purple Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-purple",
+        icon = "__DiscoLamps__/graphics/small-lamp-purple.png",
+        flags = {"goes-to-quickbar"},
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-purple]",
+        place_result = "small-lamp-purple",
+        stack_size = 50
+    },
+    -- Brown Lamp Item
+    {
+        type = "item",
+        name = "small-lamp-brown",
+        icon = "__DiscoLamps__/graphics/small-lamp-brown.png",
+        flags = {"goes-to-quickbar"},
+        subgroup = "circuit-network",
+        order = "a[light]-a[small-lamp-brown]",
+        place_result = "small-lamp-brown",
+        stack_size = 50
+    }
+})
---
+++ prototypes/recipe.lua
@@ -0,0 +1,172 @@
+local craftDuration = 0.5
+
+data:extend({
+    -- Small Red Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-red-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-red"
+    },
+    -- Small Blue Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-blue-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-blue"
+    },
+    -- Small Green Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-green-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-green"
+    },
+    -- Small Lime Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-lime-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-lime"
+    },
+    -- Small Yellow Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-yellow-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-yellow"
+    },
+    -- Small Pink Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-pink-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-pink"
+    },
+    -- Small Cyan Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-cyan-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-cyan"
+    },
+    -- Small Grey Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-grey-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-grey"
+    },
+    -- Small Black Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-black-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-black"
+    },
+    -- Small Orange Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-orange-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-orange"
+    },
+    -- Small Purple Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-purple-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-purple"
+    },
+    -- Small Brown Lamp Recipe
+    {
+        type = "recipe",
+        name = "small-lamp-brown-c",
+        energy_required = craftDuration,
+        enabled = true,
+        ingredients =
+        {
+            {"electronic-circuit", 1},
+            {"iron-stick", 3},
+            {"iron-plate", 1}
+        },
+        result = "small-lamp-brown"
+    }
+})
