3e7a0cc33d40a914dd4a4837194e3b56cbc8ece9
tehUsual <haugland54@gmail.com>
2017-09-27 20:47:48 +0200

    remaining 3(/12) lamp entities added

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -439,4 +439,92 @@ data:extend({
             shift = { -0.03125, -0.03125 },
         }
     },
+
+    -- Small Purple Lamp Entity
+    {
+        type = "lamp",
+        name = "small-lamp-purple",
+        icon = "__DiscoLamps__/graphics/icon/small-lamp-purple.png",
+        flags = { "placeable-neutral", "player-creation" },
+        minable = { hardness = 0.2, mining_time = 0.5, result = "small-lamp-purple" },
+        max_health = 100,
+        corpse = "small-remnants",
+        collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } },
+        selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+        vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+        energy_source = {
+            type = "electric",
+            usage_priority = "secondary-input"
+        },
+        energy_usage_per_tick = "5KW",
+        -- RGB Purple (164, 25, 251)
+        light = { intensity = 0.9, size = 40, color = { r = 0.643, g = 0.098, b = 0.984 } },
+        light_when_colored = { intensity = 1, size = 6, color = { r = 0.643, g = 0.098, b = 0.984 } },
+        glow_size = 6,
+        glow_color_intensity = 0.135,
+        picture_off = {
+            filename = "__DiscoLamps__/graphics/lamp-off/light-off-purple.png",
+            priority = "high",
+            width = 67,
+            height = 58,
+            frame_count = 1,
+            axially_symmetrical = false,
+            direction_count = 1,
+            shift = { -0.015625, 0.15625 },
+        },
+        picture_on = {
+            filename = "__DiscoLamps__/graphics/lamp-on/light-on-patch-purple.png",
+            priority = "high",
+            width = 62,
+            height = 62,
+            frame_count = 1,
+            axially_symmetrical = false,
+            direction_count = 1,
+            shift = { -0.03125, -0.03125 },
+        }
+    },
+
+    -- Small Brown Lamp Entity
+    {
+        type = "lamp",
+        name = "small-lamp-brown",
+        icon = "__DiscoLamps__/graphics/icon/small-lamp-brown.png",
+        flags = { "placeable-neutral", "player-creation" },
+        minable = { hardness = 0.2, mining_time = 0.5, result = "small-lamp-brown" },
+        max_health = 100,
+        corpse = "small-remnants",
+        collision_box = { { -0.15, -0.15 }, { 0.15, 0.15 } },
+        selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+        vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+        energy_source = {
+            type = "electric",
+            usage_priority = "secondary-input"
+        },
+        energy_usage_per_tick = "5KW",
+        -- RGB Brown (157, 87, 23)
+        light = { intensity = 0.9, size = 40, color = { r = 0.615, g = 0.341, b = 0.09 } },
+        light_when_colored = { intensity = 1, size = 6, color = { r = 0.615, g = 0.341, b = 0.09 } },
+        glow_size = 6,
+        glow_color_intensity = 0.135,
+        picture_off = {
+            filename = "__DiscoLamps__/graphics/lamp-off/light-off-brown.png",
+            priority = "high",
+            width = 67,
+            height = 58,
+            frame_count = 1,
+            axially_symmetrical = false,
+            direction_count = 1,
+            shift = { -0.015625, 0.15625 },
+        },
+        picture_on = {
+            filename = "__DiscoLamps__/graphics/lamp-on/light-on-patch-brown.png",
+            priority = "high",
+            width = 62,
+            height = 62,
+            frame_count = 1,
+            axially_symmetrical = false,
+            direction_count = 1,
+            shift = { -0.03125, -0.03125 },
+        }
+    },
 })
